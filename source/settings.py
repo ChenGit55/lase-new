c33ca9056d484cceac2caf52db9de56db6be1d2b
ChenGit55 <chenpsilva@gmail.com>
2024-07-08 10:53:18 -0300

    Remove emails variables

--- source/settings.py
+++ source/settings.py
@@ -22,8 +22,8 @@ if DEBUG == True:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_HOST_USER = config("EMAIL_USER")
-    EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+    EMAIL_HOST_USER = "chenpsilva@gmail.com"
+    EMAIL_HOST_PASSWORD = "dlwu mjmr mncc ymno"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
 
