30bdb546ce86c05bda6bc17ffa348c20300e06af
ChenGit55 <chenpsilva@gmail.com>
2024-07-08 10:23:39 -0300

    Changes the decouple email variables.

--- source/settings.py
+++ source/settings.py
@@ -123,7 +123,7 @@ if DEBUG == True:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+    EMAIL_HOST_USER = config("EMAIL_USER")
+    EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
