8daa23256bbc5c27c6d73b2e29ed680a964f2c15
ChenGit55 <chenpsilva@gmail.com>
2024-07-07 22:01:38 -0300

    Creates a success message when subimiting the contact form.

--- source/urls.py
+++ source/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view
+from .views import home_view, contact_view, success_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home_view, name="home"),
+    path("contact/", contact_view),
 ]
--- source/views.py
+++ source/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import JsonResponse
 
 
 def home_view(request):
     return render(request, "home.html", context={})
+
+
+def contact_view(request):
+    if request.method == "POST":
+        name = request.POST("name")
+        email = request.POST("email")
+        msg = request.POST("msg")
+
+        send_mail(
+            subject=f"Contact message from {name} - {email}, sent by web site.",
+            message={msg},
+            from_email=email,
+            recipient_list=["xegei_cps@hotmail.com"],
+        )
+
+        if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+            return JsonResponse({"message": "success"})
+        else:
+            return render(request, "contact.html")
+
+    context = {}
+    return render(request, "contact.html", context)
+
+
+def success_view(request):
+    return render(request, "success.html")
